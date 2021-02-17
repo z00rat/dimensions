e9e433e299d2c16f34e9e476a4acacf3d736960d
zoorat <78788887+thezoorat@users.noreply.github.com>
2021-02-17 12:33:21 +0600

    default rc.lua requiring

---
+++ dimensionsRc.lua
@@ -0,0 +1,23 @@
+print("[dimensions] starting...")
+-- awesome_mode: api-level=4:screen=on
+-- If LuaRocks is installed, make sure that packages installed through it are
+-- found (e.g. lgi). If LuaRocks is not installed, do nothing.
+pcall(require, "luarocks.loader")
+
+-- Standard awesome library
+gears = require("gears")
+awful = require("awful")
+require("awful.autofocus")
+-- Widget and layout library
+wibox = require("wibox")
+-- Theme handling library
+beautiful = require("beautiful")
+-- Notification library
+naughty = require("naughty")
+-- Declarative object management
+ruled = require("ruled")
+menubar = require("menubar")
+hotkeys_popup = require("awful.hotkeys_popup")
+-- Enable hotkeys help widget for VIM and other apps
+-- when client with a matching name is opened:
+require("awful.hotkeys_popup.keys")
