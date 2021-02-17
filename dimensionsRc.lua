5bef00c873cd126449c3e02dabab374e91e51416
zoorat <78788887+thezoorat@users.noreply.github.com>
2021-02-17 12:42:27 +0600

    requiring theme.lua

--- dimensionsRc.lua
+++ dimensionsRc.lua
@@ -21,3 +21,7 @@ hotkeys_popup = require("awful.hotkeys_popup")
 -- Enable hotkeys help widget for VIM and other apps
 -- when client with a matching name is opened:
 require("awful.hotkeys_popup.keys")
+
+
+local theme = require("theme")
+beautiful.init(theme)
